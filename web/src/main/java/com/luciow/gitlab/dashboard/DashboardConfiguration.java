eb976d6ca76ab406167f3416c62e78c74c53dd11
Mariusz Luciow <mariusz.luciow@ocado.com>
2017-02-23 15:37:20 +0100

    Added forward

--- web/src/main/java/com/luciow/gitlab/dashboard/DashboardConfiguration.java
+++ web/src/main/java/com/luciow/gitlab/dashboard/DashboardConfiguration.java
@@ -3,13 +3,24 @@ package com.luciow.gitlab.dashboard;
 import com.luciow.gitlab.dashboard.resolver.AccessTokenArgumentResolver;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
+@Controller
 @SpringBootApplication
 public class DashboardConfiguration {
+
+    @RequestMapping(value = {"/{path:[^\\.]*}",
+            "/{path:[^\\.]*}/{path:[^\\.]*}",
+            "/{path:[^\\.]*}/{path:[^\\.]*}/{path:[^\\.]*}",
+            "/{path:[^\\.]*}/{path:[^\\.]*}/{path:[^\\.]*}/{path:[^\\.]*}"})
+    public String redirect() {
+        return "forward:/";
+    }
 
     @Configuration
     public class WebConfiguration extends WebMvcConfigurerAdapter {
